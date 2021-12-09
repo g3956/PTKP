ad97dfb786793d874c0d20b29d748dc3e8fd89ec
spongy <48900732+g3956@users.noreply.github.com>
2021-12-09 14:53:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=4,5 python continual_train.py --logs-dir /home/wenhang/Projects/LReID_release/logs/ours
+CUDA_VISIBLE_DEVICES=4,5 python continual_train.py --logs-dir /home/wenhang/PTKP/logs/ours
